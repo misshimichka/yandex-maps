5c86f13c6f72650039204580d0beae5784354382
taisia <chegodaeva.stacy@gmail.com>
2022-02-02 17:58:25 +0300

    изменен файл с заданием 3

--- main_task3.py
+++ main_task3.py
@@ -8,8 +8,9 @@ from find_organisation import *
 
 toponym_to_find = " ".join(sys.argv[1:])
 long, lat, delta1, delta2 = find_geocoder(toponym_to_find)
+count = 1
 
-response = find_organisation(long, lat, delta1, delta2)
+response = find_organisation(long, lat, delta1, delta2, count)
 
 Image.open(BytesIO(
     response.content)).show()
