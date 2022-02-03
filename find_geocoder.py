f0aac5189e6a9cbac30f8598fbf3021585220db2
taisia <chegodaeva.stacy@gmail.com>
2022-02-03 07:21:29 +0300

    добавлена функция по нахождению района

--- find_geocoder.py
+++ find_geocoder.py
@@ -1,27 +1,62 @@
 import requests
+from distance import *
 
 
-def find_geocoder(address):
-    geocoder_api_server = "http://geocode-maps.yandex.ru/1.x/"
+def find_organisation(long, lat, delta1, delta2, n):
+    search_api_server = "https://search-maps.yandex.ru/v1/"
 
-    geocoder_params = {
-        "apikey": "40d1649f-0493-4b70-98ba-98533de7710b",
-        "geocode": address,
-        "format": "json"}
+    search_params = {
+        "apikey": "dda3ddba-c9ea-4ead-9010-f43fbc15c6e3",
+        "text": "аптека",
+        "lang": "ru_RU",
+        "ll": f"{long},{lat}",
+        "type": "biz"
+    }
 
-    response = requests.get(geocoder_api_server, params=geocoder_params)
-
+    response = requests.get(search_api_server, params=search_params)
     if not response:
         pass
 
     json_response = response.json()
-    toponym = json_response["response"]["GeoObjectCollection"][
-        "featureMember"][0]["GeoObject"]
-    coords = toponym["Point"]["pos"]
-    long, lat = coords.split(" ")
-    bbox1 = toponym["boundedBy"]["Envelope"]["lowerCorner"].split()
-    bbox2 = toponym["boundedBy"]["Envelope"]["upperCorner"].split()
 
-    delta1 = "{:.3f}".format(abs(float(bbox1[0]) - float(bbox2[0])))
-    delta2 = "{:.3f}".format(abs(float(bbox1[1]) - float(bbox2[1])))
-    return long, lat, delta1, delta2
+    points = many_organisations(json_response["features"][:n])
+
+    org = json_response["features"][0]
+
+    point = org["geometry"]["coordinates"]
+    org_point = "{0},{1}".format(point[0], point[1])
+    delta = "0.005"
+
+    distance = lonlat_distance([float(long), float(lat)], [float(point[0]), float(point[1])])
+
+    if len(points) == 1:
+        org_name = org["properties"]["CompanyMetaData"]["name"]
+        org_address = org["properties"]["CompanyMetaData"]["address"]
+        org_hours = org["properties"]["CompanyMetaData"]["Hours"]["text"]
+        print(org_name, org_address, org_hours, round(distance), sep='\n')
+
+    map_params = {
+        "l": "map",
+        "pt": f"{long},{lat},pm2rdl~" + points,
+        "z": "13"
+    }
+
+    map_api_server = "http://static-maps.yandex.ru/1.x/"
+    return requests.get(map_api_server, params=map_params)
+
+
+def many_organisations(organisations):
+    points = []
+    for org in organisations:
+        if "Hours" in org["properties"]["CompanyMetaData"].keys():
+            if len(org["properties"]["CompanyMetaData"]["Hours"]["Availabilities"][0]) == 2:
+                point = org["geometry"]["coordinates"]
+                org_point = "{0},{1},pm2gnm".format(point[0], point[1])
+            else:
+                point = org["geometry"]["coordinates"]
+                org_point = "{0},{1},pm2blm".format(point[0], point[1])
+        else:
+            point = org["geometry"]["coordinates"]
+            org_point = "{0},{1},pm2grm".format(point[0], point[1])
+        points.append(org_point)
+    return "~".join(points)
